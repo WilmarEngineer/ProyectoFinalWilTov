7ffe0fe58f5f72c22e2afc9e98fe7b01d1cf6264
Wilmar Tovar <wtovar@uniempresarial.edu.co>
2026-09-02 21:02:29 -0500

    Importacion de atributos con modificador de acceso protected con el fin de que subclases puedan acceder a atributos

--- src/proyectofinalwiltov/SuperHerore.java
+++ src/proyectofinalwiltov/SuperHerore.java
@@ -3,5 +3,8 @@ package proyectofinalwiltov;
 
 
 public class SuperHerore {
-    
+    protected String nombre= "";
+    protected int edad=0;
+    protected boolean mataCiviles=false;
+    protected double altura=0;
 }
