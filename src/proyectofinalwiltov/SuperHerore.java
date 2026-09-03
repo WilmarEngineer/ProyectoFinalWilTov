aa6e4c55d5fba48c10a765e1da73ce1ef390e480
Wilmar Tovar <wtovar@uniempresarial.edu.co>
2026-09-02 21:14:02 -0500

    Creacion de metodos getters y setters para Superclase Superheroe

--- src/proyectofinalwiltov/SuperHerore.java
+++ src/proyectofinalwiltov/SuperHerore.java
@@ -7,4 +7,29 @@ public class SuperHerore {
     protected int edad=0;
     protected boolean mataCiviles=false;
     protected double altura=0;
+    
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    public boolean getMataCiviles(){
+        return mataCiviles;
+    }
+    public void setMataCiviles(boolean mataCiviles){
+        this.mataCiviles = mataCiviles;
+    }
+    public double getAltura(){
+        return altura;
+    }
+    public void setAltura(double altura){
+        this.altura = altura;
+    }
 }
