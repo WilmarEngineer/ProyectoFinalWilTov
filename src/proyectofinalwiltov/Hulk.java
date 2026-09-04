7f791c36a6227f9ffc7cbc1f43f0e07da43049de
Wilmar Tovar <wtovar@uniempresarial.edu.co>
2026-09-03 23:10:53 -0500

    Creacion de metodos getters y setters para la Subclase Hulk

--- src/proyectofinalwiltov/Hulk.java
+++ src/proyectofinalwiltov/Hulk.java
@@ -8,5 +8,29 @@ public class Hulk extends SuperHeroe{
     private boolean mantieneControlMental=false;
     private long energiaLiberada=0;
     
+    public String getTipoDeHulk(){
+        return tipoDeHulk;
+    }
+    public void setTipoDeHulk(String tipoDeHulk){
+        this.tipoDeHulk = tipoDeHulk;
+    }
+    public int getNivelDeFuria(){
+        return nivelDeFuria;
+    }
+    public void setNivelDeFuria(int nivelDeFuria){
+        this.nivelDeFuria = nivelDeFuria;
+    }
+    public boolean getMantieneControlMental(){
+        return mantieneControlMental;
+    }
+    public void setMantieneControlMental(boolean mantieneControlMental){
+        this.mantieneControlMental = mantieneControlMental;
+    }
+    public long getEnergiaLiberada(){
+        return energiaLiberada;
+    }
+    public void setEnergiaLiberada(long energiaLiberada){
+        this.energiaLiberada = energiaLiberada;
+    }
     
 }
