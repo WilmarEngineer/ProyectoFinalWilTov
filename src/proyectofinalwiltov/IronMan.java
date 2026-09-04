e7f6164cd0ac8fc91c54a97df61b82fd5f3312cd
Wilmar Tovar <wtovar@uniempresarial.edu.co>
2026-09-03 21:30:35 -0500

    Creacion de metodos con modificador de acceso privado para Subclase Ironman

--- src/proyectofinalwiltov/IronMan.java
+++ src/proyectofinalwiltov/IronMan.java
@@ -3,5 +3,9 @@ package proyectofinalwiltov;
 
 
 public class IronMan extends SuperHeroe{
+    private String creadorTrajeIronman="";
+    private int version =0;
+    private boolean volador=true;
+    private long precioTraje=0;
     
 }
