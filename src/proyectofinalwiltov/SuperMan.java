2cad58c38f6290786d4cd72d109a9452ceb4a58e
Wilmar Tovar <wtovar@uniempresarial.edu.co>
2026-09-03 22:43:09 -0500

    Creacion de atributos privados y unicos para la Subclase Superman

--- src/proyectofinalwiltov/SuperMan.java
+++ src/proyectofinalwiltov/SuperMan.java
@@ -3,5 +3,9 @@ package proyectofinalwiltov;
 
 
 public class SuperMan extends SuperHeroe{
+    private String nombreKryptoniano="";
+    private int aniosEntrenamiento=0;
+    private boolean viajaEnElTiempo = false;
+    private long distanciaMaximaVuelo=0;
     
 }
