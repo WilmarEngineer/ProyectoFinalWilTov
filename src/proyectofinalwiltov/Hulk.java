33ded562c96292362bf53acd4234e9c6c3afbb15
Wilmar Tovar <wtovar@uniempresarial.edu.co>
2026-09-03 23:06:21 -0500

    Creacion de atributos privados para Subclase Hulk que hereda de Superclase Superheroe

--- src/proyectofinalwiltov/Hulk.java
+++ src/proyectofinalwiltov/Hulk.java
@@ -3,5 +3,10 @@ package proyectofinalwiltov;
 
 
 public class Hulk extends SuperHeroe{
+    private String tipoDeHulk="";
+    private int nivelDeFuria=0;
+    private boolean mantieneControlMental=false;
+    private long energiaLiberada=0;
+    
     
 }
