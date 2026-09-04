ea224c2f9cadab985d54c231b21309523c481ab0
Wilmar Tovar <wtovar@uniempresarial.edu.co>
2026-09-03 21:52:00 -0500

    Creacion de metodos constructores sobrecargados para Subclase Ironman

--- src/proyectofinalwiltov/IronMan.java
+++ src/proyectofinalwiltov/IronMan.java
@@ -8,4 +8,24 @@ public class IronMan extends SuperHeroe{
     private boolean volador=true;
     private long precioTraje=0;
     
+   public IronMan() {
+        this("", 0, false, 0L, "", 0, false, 0.0);
+   }
+    public IronMan(String creadorTrajeIronman, int version, boolean volador, long precioTraje, String nombre, int edad, boolean mataCiviles, double altura) {
+        super(nombre, edad, mataCiviles, altura);
+        this.creadorTrajeIronman = creadorTrajeIronman;
+        this.version = version;
+        this.volador = volador;
+        this.precioTraje = precioTraje;
+    }
+    public IronMan(String creadorTrajeIronman, int version, boolean volador, long precioTraje, String nombre, int edad, boolean mataCiviles) {
+        this(creadorTrajeIronman, version, volador, precioTraje, nombre, edad, mataCiviles, 0.0);
+    }
+    public IronMan(String creadorTrajeIronman, int version, long precioTraje, String nombre, boolean mataCiviles) {
+        this(creadorTrajeIronman, version, true, precioTraje, nombre, 0, mataCiviles, 0.0);
+    }
+    public IronMan(long precioTraje, String nombre, boolean mataCiviles) {
+        this("", 0, true, precioTraje, nombre, 0, mataCiviles, 0.0);
+    }
+    
 }
